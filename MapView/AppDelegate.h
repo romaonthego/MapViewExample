7ea306af99423e2547debc29e3a1ec02f383bef3
Roman Efimov <romaonthego@gmail.com>
2012-10-04 18:18:23 -0500

    Initial Commit

---
+++ MapView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MapView
+//
+//  Created by Roman Efimov on 10/4/12.
+//  Copyright (c) 2012 Roman Efimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
